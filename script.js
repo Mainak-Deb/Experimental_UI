a37d79f25e388dc10e052c1d75493f62098da613
debmainak <debmainak37@gmail.com>
2021-05-28 03:18:48 +0530

    triangle and flower added

--- script.js
+++ script.js
@@ -124,3 +124,44 @@ window.onload = ageCalc();
 function convertDigitIn(str) {
   return str.split('/').reverse().join('/');
 }
+
+function updateCounter(){
+  fetch('https://api.countapi.xyz/update/mainakportfolio/8be99649-2e88-4e49-8276-a0cde7a7e295/?amount=1')
+  .then(response => {return response.json()})
+  .then((data) => {
+      document.getElementById('count').innerHTML = data.value;
+      console.log( data)
+    }
+  )
+}
+updateCounter();
+ // Selecting all of the css classes 
+        // on which we want to apply functionalities
+        const hr = document.querySelector('.hr')
+        const min = document.querySelector('.min')
+        const sec = document.querySelector('.sec')
+  
+        // Setting up the period of working
+        setInterval(() => {
+  
+            // Extracting the current time 
+            // from DATE() function
+            let day = new Date()
+            let hour = day.getHours()
+            let minutes = day.getMinutes()
+            let seconds = day.getSeconds()
+  
+            // Formula that is explained above for 
+            // the rotation of different hands
+            let hrrotation = (30 * hour) + (0.5 * minutes);
+            let minrotation = 6 * minutes;
+            let secrotation = 6 * seconds;
+  
+            hr.style.transform =
+                `translate(-50%,-100%) rotate(${hrrotation}deg)`
+            min.style.transform =
+                `translate(-50%,-100%) rotate(${minrotation}deg)`
+            sec.style.transform =
+                `translate(-50%,-85%) rotate(${secrotation}deg)`
+        });
+

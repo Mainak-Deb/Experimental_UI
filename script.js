5fe6e951603c81fb683b17c3375b84b7a8a5091b
debmainak <debmainak37@gmail.com>
2021-06-14 21:33:00 +0530

    some project images added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var colors=[
 
 ]
 
-var pageid= 0;
+var pageid= 3;
 
 var skillset={
   C:"The C programming language is a computer programming language that was developed to do system programming for the operating system UNIX and is an imperative programming language. C was developed in the early 1970s by Ken Thompson and Dennis Ritchie at Bell Labs. ... C is a compiled language.",
